08f7b74eb3daf6285e3e351e22816e96d2188c3b
DimaDeveloperX <dima.podst2014@gmail.com>
2024-08-08 13:32:53 +0300

    Initial commit

---
+++ Assets/Car.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+
+    public float _carSpeed;
+
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * _carSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * _carSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * _carSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * _carSpeed * Time.deltaTime);
+        }
+    }
+   //qwertydima
+}
